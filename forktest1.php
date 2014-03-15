94c97838b1d310111d7e63b1df073390dd2e69b9
robbmj <michael.robb.prog@gmail.com>
2014-03-15 12:32:25 -0400

    A simple PHP forking program program

---
+++ forktest1.php
@@ -0,0 +1,41 @@
+#! /home/robbmi/myprograms/php/phpbuild/php-5.4.26/sapi/cli/php -q
+<?php
+
+declare(ticks = 1);
+
+function do_job($job) {
+	echo "child process: executing job $job\n";
+	sleep(2);
+	die("child process: job $job done\n");
+}
+
+function main() {
+
+	$child_pids = array();
+
+	pcntl_signal(SIGCHLD, function ($signo) use (&$child_pids) {
+		echo "parent process: got signo: $signo\n";
+		array_pop($child_pids);
+	});
+
+	foreach (range(0, 9) as $job) {
+
+		$pid = pcntl_fork();
+
+		if ($pid === 0) {
+			do_job($job);
+		}
+		else {
+			echo "parent process: started child process pid: $pid with job: $job\n";
+			array_push($child_pids, $pid);
+		}
+	}
+
+	while (count($child_pids) > 0);
+
+	echo "parrent process: all jobs done!\n";
+
+}
+
+main();
+?>
